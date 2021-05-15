96e247daa0a75ca3c268611e04f970a24dfbbe8f
Dirofu <if-sd@mail.ru>
2021-05-16 04:46:55 +0700

    Init

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private Block _templateBlock;
+    [SerializeField] private Transform _spawnPoint;
+    [SerializeField] private float _maxDropDelay;
+    
+    private float _dropDelay;
+    private Block _currentBlock;
+
+    private Vector2 _mousePosition;
+    private Vector2 _worldPosition;
+
+    private void Start()
+    {
+        _currentBlock = SpawnNewRandomBlock();
+        _currentBlock.transform.position = _spawnPoint.position;
+    }
+
+    private void Update()
+    {
+        _dropDelay -= Time.deltaTime;
+
+        if (_dropDelay <= 0)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                _mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+                _worldPosition = Camera.main.ScreenToWorldPoint(_mousePosition);
+                _currentBlock.transform.position = new Vector2(_worldPosition.x, _currentBlock.transform.position.y);
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                _currentBlock.Falling();
+                _currentBlock = SpawnNewRandomBlock();
+                _currentBlock.transform.position = _spawnPoint.position;
+
+                _dropDelay = _maxDropDelay;
+            }
+        }
+    }
+
+    private Block SpawnNewRandomBlock() => Instantiate(_templateBlock);
+}
---
+++ Assets/Scripts/Scene/General.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class General : MonoBehaviour
+{
+    [SerializeField] private GameObject _startGamePanel;
+    [SerializeField] private GameObject _endGamePanel;
+
+    private float _maxTimeCollision = 1f;
+    private float _timePassedCollision;
+
+    private int _currentScene;
+
+    private void Start()
+    {
+        _currentScene = SceneManager.GetActiveScene().buildIndex;
+
+        Time.timeScale = 0;
+        _startGamePanel.SetActive(true);
+        _endGamePanel.SetActive(false);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Block>().IsStay)
+        {
+            _timePassedCollision -= Time.deltaTime;
+
+            if(_timePassedCollision <= 0)
+            {
+                Time.timeScale = 0;
+                _endGamePanel.SetActive(true);
+            }            
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _timePassedCollision = _maxTimeCollision;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(_currentScene);
+    }
+
+    public void CloseStartGamePanel()
+    {
+        Time.timeScale = 1;
+        _startGamePanel.SetActive(false);
+    }
+}
